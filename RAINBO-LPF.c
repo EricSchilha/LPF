96c93dff4655afb52e24c903c381eed6dafda31a
schie3227 <schie3227@FHC-IC0037270.wrdsb.ca>
2017-12-08 16:50:27 -0500

    Lights mostly working (some always on, not fully scaled)

--- RAINBO-LPF.c
+++ RAINBO-LPF.c
@@ -36,13 +36,18 @@ unsigned char adConvert(unsigned char chan) {
 }
 
 void lights(unsigned char height, unsigned char leds) {
-    if (height < 247) {
-        height = (height / 4) - 31;
-    } else {
-        height = 8;
-    }
+//    if (height < 247) {
+//        if(height > 128){
+//            height = (height/4)-31;
+//        } else {
+//            height = 0;
+//        }
+//    } else {
+//        height = 30;
+//    }
+    height = height-224; 
     for (leds; leds != 0; leds--) {
-        temp = (leds < height) ? green : 0;
+        temp = (leds <= height) ? green : 0;
         for (i = 8; i != 0; i--) {
             asm("bsf LATA,4");
             asm("nop");
@@ -86,35 +91,31 @@ int main(void) {
         max3 = 0;
         max4 = 0;
         tempIN = adConvert(LPF);
-        if (tempIN > 127) {
+        if (tempIN > 222) {
             LPFInput[lastVal] = tempIN;
             lastVal = (lastVal == 9) ? 0 : lastVal + 1;
         }
-//        for (i = 0; i < 10; i++) {
-//            if (LPFInput[i] > max1) {
-//                max4 = max3;
-//                max3 = max2;
-//                max2 = max1;
-//                max1 = LPFInput[i];
-//            }
-//            if (LPFInput[i] > max2) {
-//                max4 = max3;
-//                max3 = max2;
-//                max2 = LPFInput[i];
-//            }
-//            if (LPFInput[i] > max3) {
-//                max4 = max3;
-//                max3 = LPFInput[i];
-//            }
-//            if (LPFInput[i] > max4) {
-//                max4 = LPFInput[i];
-//            }
-//        }
-        LPFAvg = 0;
-        for(i = 0; i != 10; i++){
-            LPFAvg += LPFInput[i];
+        for (i = 0; i < 10; i++) {
+            if (LPFInput[i] > max1) {
+                max4 = max3;
+                max3 = max2;
+                max2 = max1;
+                max1 = LPFInput[i];
+            }
+            if (LPFInput[i] > max2) {
+                max4 = max3;
+                max3 = max2;
+                max2 = LPFInput[i];
+            }
+            if (LPFInput[i] > max3) {
+                max4 = max3;
+                max3 = LPFInput[i];
+            }
+            if (LPFInput[i] > max4) {
+                max4 = LPFInput[i];
+            }
         }
-        LPFAvg/=10;
+        LPFAvg = (max1 + max2 + max3 + max4) / 4;
         lights(LPFAvg, maxLEDs);
         __delay_ms(10);
     }
